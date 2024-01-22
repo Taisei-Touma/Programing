04697641777abd7d7e2f3f73f4ad6c128df7316d
Taisei-Touma <138739388+Taisei-Touma@users.noreply.github.com>
2024-01-22 12:12:00 +0900

    １月２２日
    RankingDatacpp2ページまで

--- Game/DriveAndAvoid/Object/RankingData.cpp
+++ Game/DriveAndAvoid/Object/RankingData.cpp
@@ -1,1 +1,47 @@
 #include "RankingData.h"
+#include <stdio.h>
+#include <string.h>
+
+RankingData::RankingData()
+{
+	for (int i = 0; i < 6; i++)
+	{
+		score[i] = NULL;
+		rank[i] = NULL;
+		for (int j = 0; j < 15; j++)
+		{
+			name[i][j] = '\n';
+		}
+	}
+}
+
+RankingData::~RankingData()
+{
+
+}
+
+//初期化処理
+void RankingData::Initialize()
+{
+	//ランキングデータの読み込み
+	FILE* fp = nullptr;
+
+	//ファイルオープン
+	errno_t result = fopen_s(&fp, "Resource/dat/ranking_data.csv", "r");
+
+	//エラーチェック
+	if (result != 0)
+	{
+		throw ("Resource/dat/ranking_data.csvが開けませんでした\n");
+	}
+
+	//対象ファイルから読み込む
+	for (int i = 0; i < 5; i++)
+	{
+		fscanf_s(fp, "%6d, %2d, %[^,], \n", &score[i], &rank[i], name[i], 15);
+	}
+
+	//ファイルクローズ
+
+
+}
