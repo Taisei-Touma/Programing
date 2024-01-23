3020d15f64df40c2f7cfb9028411dfc76c6cf268
Taisei-Touma <138739388+Taisei-Touma@users.noreply.github.com>
2024-01-23 11:13:20 +0900

    1月23日
    SceneBaseのヘッダーまでかいた

--- Game/DriveAndAvoid/Object/RankingData.cpp
+++ Game/DriveAndAvoid/Object/RankingData.cpp
@@ -42,6 +42,103 @@ void RankingData::Initialize()
 	}
 
 	//ファイルクローズ
+	fclose(fp);
 
+	//末尾データの設定
+	score[5] = 0;
+	rank[5] = 0;
+	name[5][0] = '\0';
+}
 
+//終了処理
+void RankingData::Finalize()
+{
+
+}
+
+//データ設定処理
+void RankingData::SetRankingData(int score, const char* name)
+{
+	this->score[5] = score;
+	strcpy_s(this->name[5], name);
+
+	SortData();
+}
+
+//スコア取得処理
+int RankingData::GetScore(int value) const
+{
+	return score[value];
+}
+
+//ランク取得処理
+int RankingData::GetRank(int value) const
+{
+	return rank[value];
+}
+
+//名前取得処理
+const char* RankingData::GetName(int value) const
+{
+	return name[value];
+}
+
+//データ入れ替え処理
+void RankingData::SortData()
+{
+	//選択法ソートを使用し、降順で入れ替える
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = i + 1; j < 6; j++)
+		{
+			if (score[i] <= score[j])
+			{
+				int tmp = score[i];
+				score[i] = score[j];
+				score[j] = tmp;
+
+				char buf[15] = {};
+				strcpy_s(buf, name[i]);
+				strcpy_s(name[i], name[j]);
+				strcpy_s(name[j], buf);
+			}
+		}
+	}
+
+	//降順を整列させる
+	for (int i = 0; i < 5; i++)
+	{
+		rank[i] = 1;
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = i + 1; j < 6; j++)
+		{
+			if (score[i] > score[j])
+			{
+				rank[j]++;
+			}
+		}
+	}
+
+	//ランキングデータの書き込み
+	FILE* fp = nullptr;
+
+	//ファイルオープン
+	errno_t result = fopen_s(&fp, "Resource/dat/ranking_data.csv", "w");
+
+	//エラーチェック
+	if (result != 0)
+	{
+		throw("Resource/dat/ranking_data.csvが開けませんでした\n");
+	}
+
+	//対象ファイルに書き込み
+	for (int i = 0; i < 5; i++)
+	{
+		fprintf(fp, "%d,%d,%s,\n", score[i], rank[i], name[i]);
+	}
+
+	//ファイルクローズ
+	fclose(fp);
 }
