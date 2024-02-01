48e60eeab43d9f33a560503be301669a698ba49b
Taisei-Touma <138739388+Taisei-Touma@users.noreply.github.com>
2024-02-01 15:20:30 +0900

    ２月１日
    TitleScenecppの2ページまでかいた
    次は3ページから

--- Game/DriveAndAvoid/Scene/TitleScene.cpp
+++ Game/DriveAndAvoid/Scene/TitleScene.cpp
@@ -1,1 +1,82 @@
 #include "TitleScene.h"
+#include "../Utility/InputControl.h"
+#include "DxLib.h"
+
+TitleScene::TitleScene() : background_image(NULL), menu_image(NULL),
+cursor_image(NULL), menu_cursor(0)
+{
+
+}
+
+TitleScene::~TitleScene()
+{
+
+}
+
+//初期化処理
+void TitleScene::Initialize()
+{
+	//画像の読み込み
+	background_image = LoadGraph("Resource/images/Title.bmp");
+	menu_image = LoadGraph("Resource/images/menu.bmp");
+	cursor_image = LoadGraph("Resource/images/cone.bmp");
+
+	//エラーチェック
+	if (background_image == -1)
+	{
+		throw("Resource/images/Title.bmpがありません\n");
+	}
+	if (menu_image == -1)
+	{
+		throw("Resource/images/menu.bmpがありません\n");
+	}
+
+	if (cursor_image == -1)
+	{
+		throw("Resource/images/cone.bmpがありません\n");
+	}
+}
+
+//更新処理
+eSceneType TitleScene::Update()
+{
+	//カーソル下移動
+	if (InputControl::GetButtonDown(XINPUT_BUTTON_DPAD_DOWN))
+	{
+		menu_cursor++;
+		//１番下に到達したら、一番上にする
+		if (menu_cursor > 3)
+		{
+			menu_cursor = 0;
+		}
+	}
+
+	//カーソル上移動
+	if (InputControl::GetButtonDown(XINPUT_BUTTON_DPAD_UP))
+	{
+		menu_cursor--;
+		//１番上に到達したら、一番下にする
+		if (menu_cursor < 0)
+		{
+			menu_cursor = 3;
+		}
+	}
+
+	//カーソル決定 (決定した画面に遷移する)
+	if (InputControl::GetButtonDown(XINPUT_BUTTON_B))
+	{
+		switch (menu_cursor)
+		{
+		case 0:
+			return eSceneType::E_MAIN;
+		case 1:
+			return eSceneType::E_RANKING_DISP;
+		case 2:
+			return eSceneType::E_HELP;
+		default:
+			return eSceneType::E_END;
+		}
+	}
+
+	//現在の
+}
